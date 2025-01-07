714561e015429c4a484a10dca46ae594f7349005
nastark123 <vikingsiowa28@gmail.com>
2025-01-06 23:33:17 -0600

    Added operator functions to various value classes, need to integrate with evaluator class

--- include/values/ErrorValue.h
+++ include/values/ErrorValue.h
@@ -19,6 +19,21 @@ class ErrorValue : public Value {
 
         std::string get_value();
 
+        // Various operators that can be called on values (for comparison, addition, etc)
+        virtual Value* operator_equals(Value* other);
+
+        virtual Value* operator_greater(Value* other);
+
+        virtual Value* operator_less(Value* other);
+
+        virtual Value* operator_plus(Value* other);
+
+        virtual Value* operator_minus(Value* other);
+
+        virtual Value* operator_star(Value* other);
+
+        virtual Value* operator_slash(Value* other);
+
 };
 
 #endif
